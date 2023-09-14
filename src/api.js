27123d993df4a100424453d8c63ac8f0cbc3b2f9
Unejsi Muca <119629933+unejsimuca20@users.noreply.github.com>
2023-09-14 18:50:00 +0200

    upload
    uploading all my project

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "a77e27d261mshc3a586e90217b0ep1c6b27jsnf2b7838b80be",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "03d748e58697fe0aef0307e817eccd48";
